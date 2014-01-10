c620273a0ce13df84434e4093c620c02aec2ea55
Marcin Belczewski <marcin.belczewski@ihs.com>
2014-01-10 16:04:35 +0100

    mqwatch analyzer project setup

---
+++ mqwatch_logs/lib/mqwatch_analyzer.rb
@@ -0,0 +1,2 @@
+class MQWatchAnalyzer
+end
---
+++ mqwatch_logs/spec/mqwatch_analyzer_spec.rb
@@ -0,0 +1,9 @@
+require 'rspec'
+require_relative '../lib/mqwatch_analyzer'
+
+
+describe MQWatchAnalyzer do
+	it "works" do
+		(1 + 1).should == 2
+	end	
+end
