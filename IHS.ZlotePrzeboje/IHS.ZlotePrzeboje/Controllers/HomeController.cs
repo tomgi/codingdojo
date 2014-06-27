a5e961248d8e201c67d770bc0700fa675a4f2852
Filip Kucharczyk <filip.kucharczyk@ihs.com>
2014-06-27 21:48:03 +0200

    Zlote przeboje lista + glosowanie

---
+++ IHS.ZlotePrzeboje/IHS.ZlotePrzeboje/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IHS.ZlotePrzeboje.Models;
+
+namespace IHS.ZlotePrzeboje.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static IList<Proposition> _propositions = new List<Proposition>();
+        public ActionResult Index()
+        {
+            return View(_propositions.OrderBy(x=>x));
+        }
+
+        [HttpPost]
+        [ActionName("Index")]
+        public ActionResult IndexPOST(Proposition model)
+        {
+            model.Id = _propositions.Any() ? _propositions.Max(x => x.Id) + 1 : 0;
+
+            _propositions.Add(model);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult VoteUP(int id)
+        {
+            var proposition = _propositions.Single(x => x.Id == id);
+            proposition.VotesUP++;
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Player()
+        {
+            return View();
+        }
+    }
+}
---
+++ IHS.ZlotePrzeboje/IHS.ZlotePrzeboje/Models/Proposition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IHS.ZlotePrzeboje.Models
+{
+    public class Proposition : IComparable<Proposition>
+    {
+        public int Id { get; set; }
+        public string URL { get; set; }
+        public int VotesUP { get; set; }
+
+        public int CompareTo(Proposition other)
+        {
+            var votesUpCompare = other.VotesUP.CompareTo(this.VotesUP);
+            return votesUpCompare != 0 ? votesUpCompare : this.Id.CompareTo(other.Id);
+        }
+    }
+}
