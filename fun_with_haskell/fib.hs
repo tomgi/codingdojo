6f4ecb576f3ca110117bbaa8fd57a1e4417d5983
Marcin Belczewski <marcin.belczewski@ihs.com>
2014-08-07 19:27:20 +0200

    finally something that actually runs faster in parallel

---
+++ fun_with_haskell/fib.hs
@@ -0,0 +1,10 @@
+import Control.Parallel
+
+nfib :: Int -> Int
+nfib n | n <= 1 = 1
+       | otherwise = par n1 (pseq n2 (n1 + n2))
+                     where n1 = nfib (n-1)
+                           n2 = nfib (n-2)
+
+
+main = print $ nfib 40
--- fun_with_haskell/sieve.hs
+++ fun_with_haskell/sieve.hs
@@ -1,3 +1,16 @@
-primesR :: Integral a => a -> a -> [a]
-primesR a b = takeWhile (<= b) $ dropWhile (< a) $ sieve [2..]
-  where sieve (n:ns) = n:sieve [ m | m <- ns, m `mod` n /= 0 ]
+--primesR a b = takeWhile (<= b) $ dropWhile (< a) $ sieve [2..]
+import Debug.Trace
+sieve [] = []
+sieve (n:ns) = n:sieve [ m | m <- ns, m `mod` n /= 0 ]
+
+main = print $ sieve [2..5]
+
+
+
+
+
+
+
+
+
+
